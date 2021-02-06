a3ca81ae50be8075578791a75926eb81d722791f
sliucll <72225290+sliucll@users.noreply.github.com>
2021-02-06 20:47:25 +0100

    Update Smart Alex T2.R

--- Discovering Statistics Using R/CH3 The R Environment/Smart Alex T2.R
+++ Discovering Statistics Using R/CH3 The R Environment/Smart Alex T2.R
@@ -1,8 +1,15 @@
 Method<-c(rep(1, 10), rep(2, 10))
+
 Method<-factor(Method, levels = c(1:2), labels = c("Electric Shock", "Being Nice"))
+
 Gender<-c(rep(0, 5),rep(1, 5), rep(0, 5),rep(1, 5))
+
 Gender<-factor(Gender, levels = c(0:1), labels = c("Male", "Female"))
+
 Mark<-c(15,14,20,13,13,6,7,5,4,8,10,9,8,6,7,12,10,7,8,13)
+
 teachingMethodData<-data.frame(TeachingMethod, Gender, Mark)
+
 teachingMethodData
+
 write.table(teachingMethodData, "teachingMethodData.txt", sep="\t", row.names=FALSE)
