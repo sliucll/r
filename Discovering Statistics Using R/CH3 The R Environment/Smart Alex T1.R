347a984c73fb91289cce1a3a781b503eba2ad35b
sliucll <72225290+sliucll@users.noreply.github.com>
2021-02-06 20:44:19 +0100

    Create Smart Alex T1.R

---
+++ Discovering Statistics Using R/CH3 The R Environment/Smart Alex T1.R
@@ -0,0 +1,5 @@
+C:/Users/XXX/Documents/Data_files
+setwd("C:/Users/XXX/Documents/Data_iles")
+
+write.table(lecturerData, "Lecturer Data.txt", sep="\t", row.names = FALSE)
+#write.csv(lecturerData, "Lecturer Data.csv")
