9f3de06d8dd1057b1aa1e1b03ebd03775f74255c
sliucll <72225290+sliucll@users.noreply.github.com>
2021-02-06 20:41:43 +0100

    Update 3.9.4 Oliver Twisted.R

--- Discovering Statistics Using R/CH3 The R Environment/3.9.4 Oliver Twisted.R
+++ Discovering Statistics Using R/CH3 The R Environment/3.9.4 Oliver Twisted.R
@@ -4,3 +4,10 @@ v.names = "Name of Variable to contain Scores",
 timevar = "Name of Index Variable", 
 times = c(numbers representing levels of the index variable), 
 direction = "long")
+
+restructuredData<-reshape(satisfactionData, idvar = c("Person", "Gender"), 
+varying = c("Satisfaction_Base", "Satisfaction_6_Months", "Satisfaction_12_Months", "Satisfaction_18_Months"), 
+v.names = "Life_Satisfaction", 
+timevar = "Time", 
+times = c(0:3), 
+direction = "long")
