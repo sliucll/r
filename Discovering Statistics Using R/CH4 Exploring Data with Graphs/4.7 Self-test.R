2627e75b68cf437b15338ace59f0301019b7af50
sliucll <72225290+sliucll@users.noreply.github.com>
2021-02-06 21:01:09 +0100

    Update 4.7 Self-test.R

--- Discovering Statistics Using R/CH4 Exploring Data with Graphs/4.7 Self-test.R
+++ Discovering Statistics Using R/CH4 Exploring Data with Graphs/4.7 Self-test.R
@@ -1,3 +1,5 @@
+#Now we have removed the outlier in the data, try replotting the boxplot. The resulting graph should look like Figure 4.20
+
 festivalData<-festivalData[order(festivalData$day1),]
 
 library(Rcmdr)
@@ -5,4 +7,10 @@ library(Rcmdr)
 festivalHistogram <- ggplot(festivalData, aes(day1)) + opts(legend.position="none")
 festivalHistogram + geom_histogram(binwidth = 0.4) + 
 labs(x = "Hygiene (Day 1 of Festival)", y = "Frequency")
+
+#Produce boxplots for the day 2 and day 3 hygiene scores and interpret them.
+festivalBoxplot <- ggplot(festivalData, aes(gender, day2))
+festivalBoxplot + geom_boxplot() + labs(x = "Gender", y = "Hygiene (Day 2 of Festival)")
+festivalBoxplot <- ggplot(festivalData, aes(gender, day3))
+festivalBoxplot + geom_boxplot() + labs(x = "Gender", y = "Hygiene (Day 3 of Festival)")
 
